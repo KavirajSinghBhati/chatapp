7d82f367d2df7e8aa397aa9df3b5d9dd7aef732a
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-07-19 20:03:25 +0530

    displayed messages inside chat page

--- src/components/chat-window/messages/index.jsx
+++ src/components/chat-window/messages/index.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { database } from "../../../misc/firebase.config";
+import { transformToArrayWithId } from "../../../misc/helper";
+import MessageItem from "./MessageItem";
 
 const Messages = () => {
-  return <div>Messages</div>;
+  const [messages, setMessages] = useState(null);
+  const { chatId } = useParams();
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+  useEffect(() => {
+    const messagesRef = database.ref("/messages");
+    messagesRef
+      .orderByChild("roomId")
+      .equalTo(chatId)
+      .on("value", snap => {
+        const data = transformToArrayWithId(snap.val());
+        setMessages(data);
+      });
+    return () => {
+      messagesRef.off("value");
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMessages &&
+        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
 };
 
 export default Messages;
